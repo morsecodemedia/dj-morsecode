ba8a63042e96e513a3ad3010c159f9b8aed87224
Brandon Morse <brandon@morsecodemedia.com>
2026-09-23 00:11:00 -0400

    chore(radio): remove obsolete selection code

--- internal/radio/select.go
+++ internal/radio/select.go
@@ -26,33 +26,6 @@ func RandomCandidate(
 
 }
 
-// func Select(
-// 	candidates []Station,
-// 	history History,
-// 	options SelectionOptions,
-// ) (Station, bool) {
-
-// 	if len(candidates) == 0 {
-// 		return Station{}, false
-// 	}
-
-// 	for _, station := range candidates {
-
-// 		if history.ContainsRecent(
-// 			station.ID,
-// 			options.RecentLimit,
-// 		) {
-// 			continue
-// 		}
-
-// 		return station, true
-
-// 	}
-
-// 	return Station{}, false
-
-// }
-
 func Select(
 	candidates []Station,
 	history History,
